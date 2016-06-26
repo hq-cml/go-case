e938dfbd579c15401bfbde1f96ffe13e24df888f
hq <hq_cml@163.com>
2016-06-26 10:42:11 +0800

    命令行参数

---
+++ arguments/argu.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+ * 命令行参数解析主要通过flag包实现
+ *
+ * // String defines a string flag with specified name, default value, and usage string.
+ * // The return value is the address of a string variable that stores the value of the flag.
+ * func String(name string, value string, usage string) *string {
+ *	return CommandLine.String(name, value, usage)
+ * }
+ * 此外还有类似的Uint64、Float64等一系列方法，详见flag包结实
+ */
+import "flag"
+import "fmt"
+
+var infile *string = flag.String("i", "unsorted.dat", "File contains values for sorting")
+var outfile *string = flag.String("o", "sorted.dat", "File to receive sorted values")
+var algorithm *string = flag.String("a", "qsort", "Sort algorithm")
+
+func main() {
+    flag.Parse()
+    if infile != nil {
+        fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
+    }
+}
