9a98efbfb40206fa2ebaecb3336490903e03f940
hq <hq_cml@163.com>
2016-07-03 15:37:26 +0800

    tcp

---
+++ net/tcp.go
@@ -0,0 +1,63 @@
+package main
+/*
+ * golang的tcp用法
+ * 1. 直接Dial
+ * 2. DialTCP
+ */
+import (
+	"net"
+	"io"
+	"os"
+	"bytes"
+	"fmt"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func readFully(conn net.Conn) ([]byte, error) {
+	defer conn.Close()
+
+	result := bytes.NewBuffer(nil)
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		result.Write(buf[0:n])
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+	}
+	return result.Bytes(), nil
+}
+
+func head1(service string){
+	conn, err := net.Dial("tcp", service)
+	checkError(err)
+
+	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	checkError(err)
+
+	result, err := readFully(conn)
+	checkError(err)
+
+	fmt.Println(string(result))
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
+		os.Exit(1)
+	}
+	service := os.Args[1]
+
+	head1(service)
+
+	os.Exit(0)
+}
