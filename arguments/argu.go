037437b0dfbee02e983098c6dde13900bdd2a0a3
hq <hq_cml@163.com>
2016-07-03 15:23:34 +0800

    argu

--- arguments/argu.go
+++ arguments/argu.go
@@ -3,7 +3,8 @@ package main
 /*
  * 命令行参数的读取：
  *
- * 命令行参数解析主要通过flag包实现
+ * 命令行参数用os.Args获取
+ * 命令行选项及其参数解析主要通过flag包实现
  *
  * // String defines a string flag with specified name, default value, and usage string.
  * // The return value is the address of a string variable that stores the value of the flag.
@@ -13,13 +14,18 @@ package main
  * 此外还有类似的Uint64、Float64等一系列方法，详见flag包结实
  */
 import "flag"
-import "fmt"
+import (
+    "fmt"
+    "os"
+)
 
 var infile *string = flag.String("i", "unsorted.dat", "File contains values for sorting")
 var outfile *string = flag.String("o", "sorted.dat", "File to receive sorted values")
 var algorithm *string = flag.String("a", "qsort", "Sort algorithm")
 
 func main() {
+    fmt.Println(os.Args)
+    fmt.Println(len(os.Args))
     flag.Parse()
     if infile != nil {
         fmt.Println("infile =", *infile, "outfile =", *outfile, "algorithm =", *algorithm)
