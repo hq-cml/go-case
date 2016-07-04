81892a6897aaab35f640564246af38674b2530b6
hq <hq_cml@163.com>
2016-07-04 08:37:50 +0800

    json_decode

--- json/json.go
+++ json/json.go
@@ -8,6 +8,7 @@ type Book struct{
     Authors []string
     IsPublished bool
     Price float64
+    TestTag int       `json:"haha"`
 }
 
 func json_encode(){
@@ -16,6 +17,7 @@ func json_encode(){
         []string{"hanxiao", "hq"},
         true,
         9.9,
+        1,
     }
     b, err := json.Marshal(book)
     //fmt.Println(book)
@@ -25,6 +27,19 @@ func json_encode(){
         fmt.Println(string(b))
     }
 }
+
+//知道json格式的情况下
+func json_decode1(){
+    b := []byte(`{"Title":"Go 学习","Authors":["hanxiao","hq"],"IsPublished":true,"Price":9.9,"haha":12345}`)
+    var book Book
+    err := json.Unmarshal(b, &book)
+    if err != nil{
+        fmt.Println("Json解码出错:", err.Error())
+    }else{
+        fmt.Printf("%v",book)
+    }
+}
 func main(){
     json_encode()
+    json_decode1()
 }
