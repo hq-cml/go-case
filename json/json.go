f3f04dfd28b0bf5779cfdec120515af19856acab
hq <hq_cml@163.com>
2016-07-06 07:38:34 +0800

    json_decode

--- json/json.go
+++ json/json.go
@@ -8,7 +8,7 @@ type Book struct{
     Authors []string
     IsPublished bool
     Price float64
-    TestTag int       `json:"haha"`
+    TestTag int       `json:"haha"` //Tag, 在编码和解码中都会生效~
 }
 
 func json_encode(){
@@ -28,7 +28,7 @@ func json_encode(){
     }
 }
 
-//知道json格式的情况下
+//json结构已知的情况下
 func json_decode1(){
     b := []byte(`{"Title":"Go 学习","Authors":["hanxiao","hq"],"IsPublished":true,"Price":9.9,"haha":12345}`)
     var book Book
@@ -36,10 +36,26 @@ func json_decode1(){
     if err != nil{
         fmt.Println("Json解码出错:", err.Error())
     }else{
-        fmt.Printf("%v",book)
+        fmt.Printf("%v\n",book)
     }
 }
+
+//json结构未知的情况
+func json_decode2(){
+    b := []byte(`{"Str":"Go 学习","Arr":["hanxiao","hq"],"Bool":true,"Float":9.9}`)
+    var r interface{} //结构未知，则解码接受变量用一个空接口变量
+    err := json.Unmarshal(b, &r)
+    if err != nil{
+        fmt.Println("Json解码出错:", err.Error())
+    }else{
+        fmt.Printf("%v\n",r)
+    }
+
+    
+}
+
 func main(){
     json_encode()
     json_decode1()
+    json_decode2()
 }
