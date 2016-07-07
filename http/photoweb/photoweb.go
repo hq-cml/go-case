6d3ca649ae179370381ac4a75728ba81135bb4b3
hq <hq_cml@163.com>
2016-07-07 08:04:59 +0800

    view

--- http/photoweb/photoweb.go
+++ http/photoweb/photoweb.go
@@ -59,6 +59,26 @@ func uploadHandler(w http.ResponseWriter, r *http.Request){
     }
 }
 
+func isExists(path string) bool {
+    _, err := os.Stat(path)
+    if err == nil {
+        return true
+    }
+    return os.IsExist(err)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    imageId := r.FormValue("id")
+    imagePath := UPLOAD_DIR + "/" + imageId
+    if ok := isExists(imagePath); !ok {       //检查文件是否存在
+        http.NotFound(w, r)
+        return
+    }
+
+    w.Header().Set("Content-Type", "image")
+    http.ServeFile(w, r, imagePath)          //将文件读取并作为服务端的返回值
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
     io.WriteString(w, "Hello world!")
 }
@@ -66,6 +86,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
     http.HandleFunc("/hello", helloHandler)  //注册分发请求指针
     http.HandleFunc("/upload", uploadHandler)
+    http.HandleFunc("/view", viewHandler)
 
     err := http.ListenAndServe(":9527", nil)
     fmt.Println("End!")
