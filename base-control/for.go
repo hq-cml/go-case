bf2265663b792085132b5798e3972853bf670772
hq <hq_cml@163.com>
2016-07-24 18:03:36 +0800

    for break

--- base-control/for.go
+++ base-control/for.go
@@ -43,10 +43,31 @@ func demo4(){
     }
     fmt.Println()
     //注意，如果只有一个接收值，那么这个值是key，而不是value！！
+    //如果只想要value，那么应该 for _,v := range arr{ ...
     for v := range arr{
         fmt.Printf("%d ,", v)
     }
     fmt.Println()
+}
+
+//go break，普通的break只能跳出一层，但是呆了标记的break，可以跳出指定层数字
+func demo5(){
+    arr1 := []int{1,2,3,4}
+    arr2 := []int{1,2,3,4}
+
+    L1: //直接跳到外层的L之后
+    for _,v1 := range arr1{
+        //L2:
+        for _,v2 := range arr2{
+            if(v1 == 2){
+                //尝试放开注释，看看结果~
+                //break
+                break L1
+                //break L2
+            }
+            fmt.Println(v1, v2)
+        }
+    }
 }
 
 func main(){
@@ -54,4 +75,5 @@ func main(){
     demo2()
     demo3()
     demo4()
+    demo5()
 }
