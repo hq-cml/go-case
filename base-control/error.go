79709fef32a22cb89942c39d6e76e7c453cc21f3
hq <hq_cml@163.com>
2016-07-26 08:34:01 +0800

    error

--- base-control/error.go
+++ base-control/error.go
@@ -4,6 +4,9 @@ import (
     "fmt"
     "errors"
     "os"
+    "bufio"
+    "bytes"
+    "io"
 )
 
 //标准的error生成方法
@@ -13,10 +16,12 @@ func err1(){
 }
 
 //标准的error处理
-//通常的函数返回的都是error接口类型，所以
-//处理的时候通过类型断言，逐层判断error类型
+//1. 通常的函数返回的都是error接口类型，所以处理的时候通过类型断言，逐层判断error类型
+//2. 通过与预定义错误变量进行比较
 func handle_err(){
-    _, err := os.Open("/tmp/aaa")
+    file, err := os.Open("/tmp/aaa")
+    defer file.Close()
+    //逐层递进判断错误
     if err != nil{
         if pathErr, ok := err.(*os.PathError); ok{
             //os.PathError类型错误
@@ -24,7 +29,25 @@ func handle_err(){
         } else {
             fmt.Println("Unknow Error :", err)
         }
+        return
     }
+
+    r := bufio.NewReader(file)
+    var buf bytes.Buffer
+    for{
+        byteArray, _, err := r.ReadLine()
+        if err != nil {
+            //直接和预定义的错误变量进行比较
+            if err == io.EOF{
+                break
+            } else {
+                fmt.Println("Read error:", err)
+            }
+        }else{
+            buf.Write(byteArray)
+        }
+    }
+    fmt.Println("The file:", string(buf.Bytes()))
 }
 func main(){
     err1()
