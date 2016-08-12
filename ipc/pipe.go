13e388e9b650e10d3f2a0862132219615517c06c
hq <hq_cml@163.com>
2016-08-12 08:30:54 +0800

    pipeDemo2

--- ipc/pipe.go
+++ ipc/pipe.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "bufio"
     "io"
+    "bytes"
 )
 
 //最简单实例：执行echo -n "go cmd"
@@ -12,15 +13,17 @@ func pipeDemo1() {
     //创建一个exec.Cmd类型变量
     cmd0 := exec.Command("echo", "-n", "Go cmd")
 
-    //创建管道捕获命令输出
-    stdout0, err := cmd0.StdoutPipe()
+    //创建管道用于捕获命令输出
+    pipeOut0, err := cmd0.StdoutPipe()
     if err != nil{
         fmt.Println("Error: Create Pipe failed. " + err.Error())
+        return
     }
 
     //启动执行cmd0
     if err := cmd0.Start(); err != nil{
         fmt.Println("Error: cmd0 failed. " + err.Error())
+        return
     }
 
     //法一：从管道获取命令输出
@@ -32,7 +35,7 @@ func pipeDemo1() {
     //str := string(output0[0:n])
 
     //法二：创建一个带缓冲的reader，获取命令输出
-    outputBuf0 := bufio.NewReader(stdout0)
+    outputBuf0 := bufio.NewReader(pipeOut0)
     for {
         output0, isPrefix, err := outputBuf0.ReadLine()
 
@@ -41,6 +44,7 @@ func pipeDemo1() {
                 break // 结束
             }else{
                 fmt.Println("Read somthing wrong!")
+                return
             }
 
         }
@@ -59,6 +63,51 @@ func pipeDemo2(){
     //创建cmd变量
     cmd1 := exec.Command("ps", "aux")
     cmd2 := exec.Command("grep", "http")
+
+    //创建输出管道用于捕获命令输出
+    pipeOut, err := cmd1.StdoutPipe()
+    if err != nil{
+        fmt.Println("Error: Create Pipe failed. " + err.Error())
+    }
+
+    //启动执行cmd1
+    if err := cmd1.Start(); err != nil{
+        fmt.Println("Error: cmd1 failed. " + err.Error())
+    }
+
+    //为cmd2创建输入管道，并将cmd1的输出管道的内容写入前者
+    outputBuf1 := bufio.NewReader(pipeOut)
+
+    pipeIn, err := cmd2.StdinPipe()
+
+    if err != nil {
+        fmt.Println("Error in create pipe in: "+err.Error())
+    }
+    outputBuf1.WriteTo(pipeIn)
+    //err = pipeOut.Close()   //经过测试，这个close可有可无
+    //创建cmd2的输出缓冲区，这样当cmd2执行完毕之后，其输出将保存在此缓冲
+    var outputBuf2 bytes.Buffer
+    cmd2.Stdout = &outputBuf2
+
+    //执行cmd2,并关闭管道
+    if err := cmd2.Start(); err != nil{
+        fmt.Println("Error: cmd2 failed. " + err.Error())
+        return
+    }
+    err = pipeIn.Close() //这个关闭时必须的，否则下面的Wait会一直等待
+    if err != nil{
+        fmt.Println("pipeIn close failed. " + err.Error())
+        return
+    }
+
+    //等待cmd2执行完毕，如果不等待，主线程会提前结束
+    if err := cmd2.Wait(); err != nil {
+        fmt.Println("cmd wait failed. " + err.Error())
+        return
+    }
+
+    //输出结果
+    fmt.Println(outputBuf2.String())
 }
 
 func main() {
