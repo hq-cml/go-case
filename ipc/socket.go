a168fa9e5df0e46e4305f50c11c07f4751e454b1
hq <hq_cml@163.com>
2016-08-21 10:09:40 +0800

    read function

--- ipc/socket.go
+++ ipc/socket.go
@@ -4,12 +4,13 @@ import (
     "fmt"
     "net"
     "time"
+    "bytes"
 )
 
 const (
-    SERVER_PROTOCAL  = "tcp"
+    SERVER_PROTOCAL = "tcp"
     SERVER_ADDRESS  = "127.0.0.1:9527"
-    //SERVER_NETWORK  = "tcp"
+    DELIMITER       = '\t'            //定界符
 )
 
 var logSn = 1
@@ -49,6 +50,34 @@ func tcpClient(id int) {
         printLog("Dial error: %s (client[%d])", err, id)
     }
 }
+
+//read方法
+func read(conn net.Conn) (string, error) {
+    readBytes := make([]byte, 1)
+    var buffer bytes.Buffer  //bytes缓冲区，保存读取数据
+    for {
+        _, err := conn.Read(readBytes) //每次接收一个字节
+        if err != nil {
+            return "", err //交给上层去处理
+        }
+        readByte := readBytes[0]
+        if readByte == DELIMITER {
+            break
+        }
+        buffer.WriteByte(readByte)
+    }
+    return buffer.String(), nil //返回
+}
+
+// 法二：利用bufio来实现上述read方法
+//func read(conn net.Conn) (string, error) {
+//	reader := bufio.NewReader(conn)
+//	readBytes, err := reader.ReadBytes(DELIMITER)
+//	if err != nil {
+//		return "", err
+//	}
+//	return string(readBytes[:len(readBytes)-1]), nil
+//}
 
 func main() {
 
