7b9ebbf032dd14591761fd0754e83f855675d2bc
hq <hq_cml@163.com>
2016-11-14 08:41:22 +0800

    renamed

--- customized-map/ordered_map/keys.go
+++ customized-map/ordered_map/keys.go
@@ -8,13 +8,16 @@ import(
 )
 
 //函数类型声明
+//compareFunction的结果值：
+//小于0: 第一个参数小于第二个参数
+//等于0: 第一个参数等于第二个参数
+//大于0: 第一个参数大于第二个参数
 type CompareFunction func(interface{}, interface{}) int8
 
 /*
- * 定义接口一枚，作为ordered-map的keys成员的变量
- * 需要实现这个接口的方法
+ * 定义接口，作为ordered-map的keys成员的变量
  */
-type Keys interface {
+type KeysIntfs interface {
     sort.Interface                 //嵌入sort.Interface接口，意味着Keys的实现类型是可以排序的~
     Add(k interface{}) bool        //增加一个key
     Remove(k interface{}) bool     //去除一个key
@@ -29,15 +32,11 @@ type Keys interface {
 //自定义类型myKeys
 type myKeys struct {
     container    []interface{}    //keys的实际容器，keys元素可以是任意类型
-    //compareFunc的结果值：
-    //小于0: 第一个参数小于第二个参数
-    //等于0: 第一个参数等于第二个参数
-    //大于0: 第一个参数大于第二个参数
     compareFunc  CompareFunction  //函数也是一种类型，compareFunc负责比较元素的大小，具体实现交给上层开发者
     elemType     reflect.Type     //存储keys元素的实际类型，（运行时确定）
 }
 
-//让类型*myKeys实现Keys接口:
+//让类型*myKeys实现KeysIntfs接口:
 //首先，实现嵌入接口sort.Interface：Len，Less，Swap
 func (keys *myKeys) Len() int{
     return len(keys.container)
@@ -157,7 +156,7 @@ func (keys *myKeys) isAcceptableElem(k interface{}) bool {
 }
 
 //golang惯例，“构造”函数
-func NewKeys(compareFunc CompareFunction, elemType reflect.Type) Keys {
+func NewKeys(compareFunc CompareFunction, elemType reflect.Type) KeysIntfs {
     return &myKeys{
         container:    make([]interface{}, 0),
         compareFunc:  compareFunc,
