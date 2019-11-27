fd95bce50adc5a1e9b7e71ad4bb9706e1963d139
hq <hq_cml@163.com>
2019-11-27 09:01:41 +0800

    strings

--- strings/reader.go
+++ strings/reader.go
@@ -16,6 +16,8 @@ func reader() {
    buf1 := make([]byte, 47)
    n, _ := reader1.Read(buf1)
    fmt.Printf("%d bytes were read. (call Read)\n", n)
+   fmt.Printf("The size of reader: %d\n", reader1.Size())
+   fmt.Printf("The Len of reader: %d\n", reader1.Len())
    fmt.Printf("The reading index in reader: %d\n",
        reader1.Size()-int64(reader1.Len()))
    fmt.Println()
